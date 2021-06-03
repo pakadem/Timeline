aabb4086b53c58a5aa4d13a89fc84a041590a6fd
Malusi Pakade <pakadem@gmail.com>
2021-06-04 00:15:22 +0200

    fixing bugs

--- app/src/main/java/com/example/timeline/TimelineActivity.java
+++ app/src/main/java/com/example/timeline/TimelineActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -14,34 +15,11 @@ import java.util.Arrays;
 
 public class TimelineActivity extends AppCompatActivity {
     ListView listview;
-//    Context context;
-//    ArrayList tl_arrlist;
 
-     String country_names[] = {"Bostwana",
-                                "Eswatini",
-                                "Lesotho",
-                                "Malawi",
-                                "Mali",
-                                "Mozambique",
-                                "Namibia",
-                                "South Africa",
-                                "Tanzania",
-                                "Zambia",
-                                "Zimbabwe"
-                                };
+    String country_names[] = {"Bostwana","Eswatini","Lesotho","Malawi","Mali", "Mozambique","Namibia","South Africa","Tanzania", "Zambia", "Zimbabwe"};
 
-    int country_flags[] = { R.drawable.flag_botswana,
-                            R.drawable.flag_eswatini,
-                            R.drawable.flag_lesotho,
-                            R.drawable.flag_malawi,
-                            R.drawable.flag_mali,
-                            R.drawable.flag_mozambique,
-                            R.drawable.flag_nambia,
-                            R.drawable.flag_sa,
-                            R.drawable.flag_tanzania,
-                            R.drawable.flag_zambia,
-                            R.drawable.flag_zimbabwe
-                        };
+    int country_flags[] = {R.drawable.flag_botswana,R.drawable.flag_eswatini,R.drawable.flag_lesotho, R.drawable.flag_malawi, R.drawable.flag_mali, R.drawable.flag_mozambique, R.drawable.flag_nambia, R.drawable.flag_sa, R.drawable.flag_tanzania, R.drawable.flag_zambia, R.drawable.flag_zimbabwe};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,13 +31,14 @@ public class TimelineActivity extends AppCompatActivity {
 
         CustomAdapter customadapter = new CustomAdapter (getApplicationContext(), country_names, country_flags);
         listview.setAdapter(customadapter);
-        int i = country_names.length;
-        listview.setOnClickListener( new View.OnClickListener()  {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Country", Toast.LENGTH_LONG).show();
-            }
-        });
+        //int i = country_names.length;
+//        listview.setOnItemClickListener( new AdapterView.OnItemClickListener()  {
+//            @Override
+//            public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
+//                Toast.makeText(TimelineActivity.this, "Clicked on this country", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
     }
 
 }
